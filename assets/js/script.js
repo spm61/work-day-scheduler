03bb250c70d8b67887a60c1513af9814fa089f38
spmoroney <spmoroney@att.net>
2023-07-30 18:23:42 -0400

    Fix up the local storage logic to actually work.

--- assets/js/script.js
+++ assets/js/script.js
@@ -10,8 +10,9 @@ $(function () {
   // useful when saving the description in local storage?
 
   //display the current date on screen.
-  var today = dayjs('dddd, MMM Do YYYY');
-$("#currentDay").textContent = today;
+  var todayDate = dayjs().format('dddd MMMM DD, YYYY');
+$("#currentDay").html(todayDate);
+
 
   //listener on the save button
   $(".saveBtn").on("click", function() {
@@ -36,7 +37,6 @@ $("#currentDay").textContent = today;
     //iterate over each block
     $(".time-block").each(function() {
       var currentBlock = parseInt($(this).attr("id").split("hour")[1]);
-    
       //checks the current hour to determine if an hour is in the past, present or future.
       //If it's in the past add the past class, if its in the present add the present class..
       //Otherwise it's in the future class.  Only one of these three classes should be added at a time.
@@ -63,4 +63,18 @@ $("#currentDay").textContent = today;
   // attribute of each time-block be used to do this?
   //
   // TODO: Add code to display the current date in the header of the page.
+  
+  //pull the items from local storage if there are any, then invoke the time tracker function.
+  $("#hour-9 .description").val(localStorage.getItem("hour-9"));
+  $("#hour-10 .description").val(localStorage.getItem("hour-10"));
+  $("#hour-11 .description").val(localStorage.getItem("hour-11"));
+  $("#hour-12 .description").val(localStorage.getItem("hour-12"));
+  $("#hour-1 .description").val(localStorage.getItem("hour-1"));
+  $("#hour-2 .description").val(localStorage.getItem("hour-12"));
+  $("#hour-3 .description").val(localStorage.getItem("hour-3"));
+  $("#hour-4 .description").val(localStorage.getItem("hour-4"));
+  $("#hour-5 .description").val(localStorage.getItem("hour-5"));
+
+  trackTime();
+
 });
